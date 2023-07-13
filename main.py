040b78b987bf6a7d9f8246e7f2d2be96064d87d8
maki_ <ny.2151102@gmail.com>
2023-07-13 21:39:13 +0900

    修正

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ for url in urls:
         summary = response_summary['choices'][0]['message']['content']
 
         # ディスコードに送信するメッセージをフォーマット
-        message = f"🗞{website.brand}\🧳{a.title}\n{summary}\n🔗{a.url}"
+        message = f"🗞{website.brand}\n📝{a.title}\n{summary}\n🔗{a.url}\n⌐◨-◨ ⌐◨-◨ ⌐◨-◨ ⌐◨-◨ ⌐◨-◨ ⌐◨-◨\n\n"
 
         # 各ウェブフックURLに対してディスコードに送信
         for webhook_url in WEBHOOK_URLS:
